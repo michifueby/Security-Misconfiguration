918b0abbfbfdaf0df7937cc0162379befea1cba3
TUNJIC Josip <josip.tunjic77@gmail.com>
2023-04-17 15:10:41 +0200

    Added PrimeNG

--- src/frontend/versex-home-automation/src/app/app.module.ts
+++ src/frontend/versex-home-automation/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
