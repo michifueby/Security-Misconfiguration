233da7be2b30ad80a3a4e8e4a70ba2b8d599a9e6
TUNJIC Josip <josip.tunjic77@gmail.com>
2023-04-17 14:00:07 +0200

    Project Setup

---
+++ src/backend/versex-home-automation/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace versex_home_automation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
