aeecd1197717fa1d3d16bfcd33a1b700376287c8
TUNJIC Josip <josip.tunjic77@gmail.com>
2023-05-17 14:27:18 +0200

    Restored Conflict

--- src/frontend/versex-home-automation/src/app/components/controls/add-device/add-device.component.ts
+++ src/frontend/versex-home-automation/src/app/components/controls/add-device/add-device.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-device',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AddDeviceComponent {
 
+  @Output() onClicked = new EventEmitter()
+
+  click(){
+    this.onClicked.emit();
+
+  }
 }
--- src/frontend/versex-home-automation/src/app/components/pages/login-register/login.component.ts
+++ src/frontend/versex-home-automation/src/app/components/pages/login-register/login.component.ts
@@ -8,9 +8,7 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent {
-  constructor(
-  ) {
-  }
+
 }
 
 
